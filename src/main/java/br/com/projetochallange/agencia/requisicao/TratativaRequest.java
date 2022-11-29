179a03c55a969d504856f52e25218a2dbbd16b57
juliadde <juliaddevangelista@gmail.com>
2022-11-29 15:15:44 -0300

    .

--- src/main/java/br/com/projetochallange/agencia/requisicao/TratativaRequest.java
+++ src/main/java/br/com/projetochallange/agencia/requisicao/TratativaRequest.java
@@ -3,15 +3,17 @@ package br.com.projetochallange.agencia.requisicao;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @Table(name = "tratativa")
 @Entity
-public class TratativaRequest {
-    @Id
+public class TratativaRequest implements Serializable {
+
     @OneToOne
     @JoinColumn(name = "id_mensagemErro")
     private InconsistenciaRequest inconsistenciaRequest;
+    @Id
     private String procedimento;
     private Boolean statusConcluido;
     @OneToOne
